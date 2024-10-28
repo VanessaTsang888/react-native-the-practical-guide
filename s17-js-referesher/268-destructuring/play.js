1423836830bd8492385042444e8d156db149517b
Vanessa <vantaidesign8@gmail.com>
2024-10-28 13:39:06 +1100

    Updatedd code file for lecture 268

--- s17-js-referesher/268-destructuring/play.js
+++ s17-js-referesher/268-destructuring/play.js
@@ -67,18 +67,3 @@ const hobbies = ['Sports', 'Cooking'];
 const [hobby1, hobby2] = hobbies;
 // Logout two different values that we got from the Array Destructuring above.
 console.log(hobby1, hobby2); // Sports Cooking
-
-/*
-const printName = ({ name }) => {
-  console.log(name);
-};
-
-printName(person);
-
-const { name, age } = person;
-console.log(name, age);
-
-const hobbies = ['Sports', 'Cooking'];
-const [hobby1, hobby2] = hobbies;
-console.log(hobby1, hobby2);
-*/
